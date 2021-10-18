774a29776d0c2a45162a527e470fc24f0d297b9b
ovieimara <oimara@enprohsoft.com>
2021-10-18 14:00:46 +0100

    main.py update

--- jsonreader.sh
+++ jsonreader.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-#pip install pytest #comment after first run
+pip install pytest #comment after first run
+source venv/bin/activate
 python3 main.py "data/data_2.json" "schema/schema_2.json"
 python3 main.py "data/data_1.json" "schema/schema_1.json"
 
